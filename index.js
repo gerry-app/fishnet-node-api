b692a7b3f1fea0d0d3cc00245769399d760fdc6d
Jason Kao <jason.kao@columbia.edu>
2020-05-22 23:12:24 -0400

    setup for heroku

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3001;
 
 const MDB_USERNAME = process.env.MDB_USERNAME;
 const MDB_PASSWORD = process.env.MDB_PASSWORD;
